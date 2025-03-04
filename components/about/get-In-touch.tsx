4d43fb9c77a4964fa44cd5435ba6ba27e6b80337
mileshendricks <nextbit.cs@gmail.com>
2025-03-04 15:51:12 +0600

    doc:- get in touch and contact

--- components/about/get-In-touch.tsx
+++ components/about/get-In-touch.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { PrimaryButton } from "../shared/parimary-button";
 import { SecondaryButton } from "../shared/secondary-button";
 
@@ -7,12 +8,14 @@ export default function GetInTouch() {
     <div className="">
       <div className="mx-auto max-w-7xl  py-20 sm:py-28 lg:flex lg:items-center lg:justify-between ">
         <h2 className="max-w-2xl text-2xl md:text-4xl font-semibold leading-[1.2] tracking-[-0.04em] text-secondary dark:text-primary ">
-          Ready to dive in? <br />
-          Start your free trial today.
+        Sound like a good fit?<br />
+        Let's talk!
         </h2>
         <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
-          <PrimaryButton>Get Started</PrimaryButton>
-          <SecondaryButton className="w-[168px] md:w-[183px] px-[16px] py-[10px] h-10">Learn more</SecondaryButton>
+          <PrimaryButton>Book a free call</PrimaryButton>
+          <SecondaryButton className="w-[168px] md:w-[183px] px-[16px] py-[10px] h-10">
+            <Link href="/contact"> Email us</Link>
+          </SecondaryButton>
         </div>
       </div>
     </div>
